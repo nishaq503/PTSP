25dc93ea738b9c8fd3c44ddb0234db3a00e6656d
nishaq503 <najib_ishaq@my.uri.edu>
2019-01-30 20:16:04 -0500

    looking at depressing graphs

--- r_tutorial/tutorial.R
+++ r_tutorial/tutorial.R
@@ -1,2 +1,7 @@
+library(ggplot2)
+
 original_data <- read.csv(file="data/gapminder_data.csv", header=TRUE, sep=",", stringsAsFactors=FALSE)
 original_data[sample(nrow(original_data), 5), ]
+summary(original_data)
+ggplot(data=original_data, aes(x=year, y=lifeExp, color=continent)) + geom_point()
+original_data[original_data$lifeExp == min(original_data$lifeExp),]
