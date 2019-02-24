5924a9cc94b5c0b224666675b45b8113b005332d
Najib <najib_ishaq@my.uri.edu>
2019-02-24 00:04:59 -0500

    cleaned up code so it reads better. removed switch class

--- project/parse.py
+++ project/parse.py
@@ -1,132 +1,31 @@
-import re
 import sys
 
 import tensorflow as tf
 
-from project.utilities import Switch
+from project.utilities import record_to_dict, dict_to_tfrecord
 
 
-# Constants
-NUM_DIMENSIONS = 3
-
-# helpers for dealing with TF Example etc and make code more verbose
-# Accessory functions for dealing with TF Example and SequenceExample
-_example = tf.train.Example
-_sequence_example = tf.train.SequenceExample
-_feature = tf.train.Feature
-_features = lambda d: tf.train.Features(feature=d)
-_feature_list = lambda l: tf.train.FeatureList(feature=l)
-_feature_lists = lambda d: tf.train.FeatureLists(feature_list=d)
-_bytes_feature = lambda v: _feature(bytes_list=tf.train.BytesList(value=v))
-_int64_feature = lambda v: _feature(int64_list=tf.train.Int64List(value=v))
-_float_feature = lambda v: _feature(float_list=tf.train.FloatList(value=v))
-
-# Dictionaries for dealing with input data
-_aa_dict = {'A': '0', 'C': '1', 'D': '2', 'E': '3', 'F': '4', 'G': '5', 'H': '6', 'I': '7', 'K': '8', 'L': '9', 'M': '10', 'N': '11', 'P': '12', 'Q': '13', 'R': '14', 'S': '15', 'T': '16', 'V': '17', 'W': '18', 'Y': '19'}
-_dssp_dict = {'L': '0', 'H': '1', 'B': '2', 'E': '3', 'G': '4', 'I': '5', 'T': '6', 'S': '7'}
-_mask_dict = {'-': '0', '+': '1'}
-
-
-def string_to_num_list(string, dict_):
+def parse(input_path, output_path, num_entries):
     """
-    Convert a string of letters into a list of ints using the dictionary provided
-    :param string: string of letters
-    :param dict_: dictionary to use
-    :return: list of ints
+    :param input_path: path of file to read.
+    :param output_path: path of file to write.
+    :param num_entries: number of entries.
     """
-    pattern = re.compile('[' + ''.join(dict_.keys()) + ']')
-    num_string = pattern.sub(lambda m: dict_[m.group(0)] + ' ', string)
-    return [int(i) for i in num_string.split()]
-
-
-def record_to_dict(infile, num_entries):
-    """
-
-    :param infile:
-    :param num_entries:
-    :return:
-    """
-
-    dict_ = {}
-
-    while True:
-        line = infile.readline()
-        for case in Switch(line):
-            if case('[ID]' + '\n'):
-                id_ = infile.readline()[: -1]
-                dict_.update({'id': id_})
-            elif case('[PRIMARY]' + '\n'):
-                primary = string_to_num_list(infile.readline()[:-1], _aa_dict)
-                dict_.update({'primary': primary})
-            elif case('[EVOLUTIONARY]' + '\n'):
-                evolutionary = []
-                for residue in range(num_entries):
-                    evolutionary.append([float(step) for step in infile.readline().split()])
-                dict_.update({'evolutionary': evolutionary})
-            elif case('[SECONDARY]' + '\n'):
-                secondary = string_to_num_list(infile.readline()[:-1], _dssp_dict)
-                dict_.update({'secondary': secondary})
-            elif case('[TERTIARY]' + '\n'):
-                tertiary = []
-                for axis in range(NUM_DIMENSIONS):
-                    tertiary.append([float(coord) for coord in infile.readline().split()])
-                dict_.update({'tertiary': tertiary})
-            elif case('[MASK]' + '\n'):
-                mask = string_to_num_list(infile.readline()[:-1], _mask_dict)
-                dict_.update({'mask': mask})
-            elif case('\n'):
-                return dict_
-            elif case(''):
-                return None
-
-
-def dict_to_tfrecord(dict_):
-    """
-    convert a given dictionary into a tfrecord.
-    :param dict_: dictionary to convert
-    :return: TFRecord
-    """
-
-    id_ = _bytes_feature([dict_['id']])
-
-    feature_lists_dict = {}
-    feature_lists_dict.update({'primary': _feature_list([_int64_feature([aa]) for aa in dict_['primary']])})
-
-    if dict_.has_key('evolutionary'):
-        feature_lists_dict.update({'evolutionary': _feature_list([_float_feature(list(step)) for step in zip(*dict_['evolutionary'])])})
-
-    if dict_.has_key('secondary'):
-        feature_lists_dict.update({'secondary': _feature_list([_int64_feature([dssp]) for dssp in dict_['secondary']])})
-
-    if dict_.has_key('tertiary'):
-        feature_lists_dict.update({'tertiary': _feature_list([_float_feature(list(coord)) for coord in zip(*dict_['tertiary'])])})
-
-    if dict_.has_key('mask'):
-        feature_lists_dict.update({'mask': _feature_list([_float_feature([step]) for step in dict_['mask']])})
-
-    record = _sequence_example(context=_features({'id': id_}), feature_lists=_feature_lists(feature_lists_dict))
-
-    return record
-
-
-if __name__ == '__main__':
-    """
-    main.
-    Accepts 3 command line arguments.
-    :param input_path: path of file to read
-    :param output_path: path of file to write
-    :param num_entries: number of entries. defaults to 20.
-    """
-    input_path = sys.argv[1]
-    output_path = sys.argv[2]
-    num_entries = int(sys.argv[3]) if len(sys.argv) == 4 else 20
-
     with open(input_path, 'r') as infile, tf.python_io.TFRecordWriter(output_path) as outfile:
 
         while True:
             dict_ = record_to_dict(infile, num_entries)
+
             if dict_ is not None:
                 serialized_tf_record = dict_to_tfrecord(dict_).SerializeToString()
                 outfile.write(serialized_tf_record)
+
             else:
                 break
+    return
+
+
+if __name__ == '__main__':
+    file_in, file_out = sys.argv[1], sys.argv[2]
+    entries = int(sys.argv[3]) if len(sys.argv) == 4 else 20
+    parse(file_in, file_out, entries)
--- project/utilities.py
+++ project/utilities.py
@@ -1,32 +1,141 @@
+import re
+
+import tensorflow as tf
+
+# Constants
+NUM_DIMENSIONS = 3
+
+# helpers for dealing with TF Example etc and make code more verbose
+# Accessory functions for dealing with TF Example and SequenceExample
+_example = tf.train.Example
+_sequence_example = tf.train.SequenceExample
+_feature = tf.train.Feature
 
 
-class Switch(object):
+def _features(d):
+    return tf.train.Features(feature=d)
+
+
+def _feature_list(l):
+    return tf.train.FeatureList(feature=l)
+
+
+def _feature_lists(d):
+    return tf.train.FeatureLists(feature_list=d)
+
+
+def _bytes_feature(v):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=v))
+
+
+def _int64_feature(v):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=v))
+
+
+def _float_feature(v):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=v))
+
+
+def string_to_num_list(string, dict_):
     """
-    Switch statements for python
+    Convert a string of letters into a list of ints using the dictionary provided
+    :param string: string of letters
+    :param dict_: dictionary to use
+    :return: list of ints
+    """
+    pattern = re.compile('[' + ''.join(dict_.keys()) + ']')
+    num_string = pattern.sub(lambda m: dict_[m.group(0)] + ' ', string)
+    return [int(i) for i in num_string.split()]
+
+
+def record_to_dict(infile, num_entries):
     """
 
-    def __init__(self, value):
-        self.value = value
-        self.fall = False
+    :param infile: file to read
+    :param num_entries: pssm entries
+    :return:
+    """
+    # Dictionaries for dealing with input data
+    _aa_dict = {'A': '0', 'C': '1', 'D': '2', 'E': '3', 'F': '4', 'G': '5', 'H': '6', 'I': '7', 'K': '8', 'L': '9',
+                'M': '10', 'N': '11', 'P': '12', 'Q': '13', 'R': '14', 'S': '15', 'T': '16', 'V': '17', 'W': '18',
+                'Y': '19'}
+    _dssp_dict = {'L': '0', 'H': '1', 'B': '2', 'E': '3', 'G': '4', 'I': '5', 'T': '6', 'S': '7'}
+    _mask_dict = {'-': '0', '+': '1'}
 
-    def __iter__(self):
-        """
-        returns the matched method once, then stop
-        :return:
-        """
-        yield self.match
-        raise StopIteration
+    dict_ = {}
 
-    def match(self, *args):
-        """
-        indicate whether or not to enter a case
-        :param args: arguments
-        :return: bool whether or not to enter case
-        """
-        if self.fall or not args:
-            return True
-        elif self.value in args:
-            self.fall = True
-            return True
-        else:
-            return False
+    while True:
+        line = infile.readline()
+
+        if line == '[ID]' + '\n':
+            id_ = infile.readline()[: -1]
+            dict_.update({'id': id_})
+
+        elif line == '[PRIMARY]' + '\n':
+            primary = string_to_num_list(infile.readline()[:-1], _aa_dict)
+            dict_.update({'primary': primary})
+
+        elif line == '[EVOLUTIONARY]' + '\n':
+            evolutionary = []
+            for residue in range(num_entries):
+                evolutionary.append([float(step) for step in infile.readline().split()])
+            dict_.update({'evolutionary': evolutionary})
+
+        elif line == '[SECONDARY]' + '\n':
+            secondary = string_to_num_list(infile.readline()[:-1], _dssp_dict)
+            dict_.update({'secondary': secondary})
+
+        elif line == '[TERTIARY]' + '\n':
+            tertiary = []
+            for axis in range(NUM_DIMENSIONS):
+                tertiary.append([float(coord) for coord in infile.readline().split()])
+            dict_.update({'tertiary': tertiary})
+
+        elif line == '[MASK]' + '\n':
+            mask = string_to_num_list(infile.readline()[:-1], _mask_dict)
+            dict_.update({'mask': mask})
+
+        elif line == '\n':
+            return dict_
+
+        elif line == '':
+            return None
+
+
+def dict_to_tfrecord(dict_):
+    """
+    convert a given dictionary into a tfrecord.
+    :param dict_: dictionary to convert
+    :return: TFRecord
+    """
+
+    id_ = _bytes_feature([dict_['id']])
+
+    feature_lists_dict = {}
+    feature_lists_dict.update({'primary': _feature_list([_int64_feature([aa]) for aa in dict_['primary']])})
+
+    if 'evolutionary' in dict_:
+        feature_lists_dict.update(
+            {'evolutionary': _feature_list([_float_feature(list(step)) for step in zip(*dict_['evolutionary'])])}
+        )
+
+    if 'secondary' in dict_:
+        feature_lists_dict.update({'secondary': _feature_list([_int64_feature([dssp]) for dssp in dict_['secondary']])}
+                                  )
+
+    if 'tertiary' in dict_:
+        feature_lists_dict.update(
+            {'tertiary': _feature_list([_float_feature(list(coord)) for coord in zip(*dict_['tertiary'])])}
+        )
+
+    if 'mask' in dict_:
+        feature_lists_dict.update(
+            {'mask': _feature_list([_float_feature([step]) for step in dict_['mask']])}
+        )
+
+    record = tf.train.SequenceExample(
+        context=_features({'id': id_}),
+        feature_lists=_feature_lists(feature_lists_dict)
+    )
+
+    return record
