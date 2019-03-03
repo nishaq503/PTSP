011acf2ef1f50f4f41f45a5d79b445e95432f306
Najib <najib_ishaq@my.uri.edu>
2019-03-03 17:52:26 -0500

    wrote l2_norm

--- project/geometry.py
+++ project/geometry.py
@@ -32,3 +32,34 @@ def mean_angle(radii, angles, name=None):
 
         return tf.atan2(y_s, x_s, name=scope)
 
+
+def reduce_l2_norm(_input,
+                   weights=None,
+                   reduction_indexes=None,
+                   keep_dims=None,
+                   epsilon=1e-12,
+                   name=None):
+    """
+    computes the l2_norm of the input along the dimensions in reduction_indexes.
+    :param _input: tensor-like input.
+    :param weights: tensor-like weights.
+    :param reduction_indexes: which dimensions to calculate l2_norm for.
+    :param keep_dims: which dimensions to keep.
+    :param epsilon: lowest allowed precision
+    :param name: optional name for scope
+    :return: tensor of l2_norm along given dimensions
+    """
+    with tf.name_scope(name, 'reduce_l2_norm', [_input, weights]) as scope:
+        input_tensor = tf.convert_to_tensor(_input, name='_input')
+
+        input_tensor_squared = tf.square(input_tensor)
+        if weights:
+            weights_tensor = tf.convert_to_tensor(weights, name='weights')
+            input_tensor_squared *= weights_tensor
+
+        return tf.sqrt(tf.maximum(tf.reduce_sum(input_tensor_squared,
+                                                axis=reduction_indexes,
+                                                keep_dims=keep_dims),
+                                  epsilon),
+                       name=scope)
+
